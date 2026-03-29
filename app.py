fa37e36fc180a238db5dcad38f6494bb455d96e8
roselinprincy22-lab <roselinprincy22@gmail.com>
2026-03-29 14:11:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import requests
 
-ESP32_IP = "http://10.145.234.126"  # change this
+ESP32_IP = "http://10.145.234.126"
 
+# 🔥 Fix connection issue
 session = requests.Session()
 session.trust_env = False
 
@@ -10,19 +11,22 @@ st.title("🌱 Sensor Data")
 
 if st.button("Get Sensor Data"):
     try:
-        res = session.get(f"{ESP32_IP}/data", timeout=5)
+        res = session.get(f"{ESP32_IP}/data", timeout=10)
 
         if res.status_code == 200:
             data = res.json()
             soil = data["soil"]
 
-            st.success("Connected ✅")
+            st.success("Connected to ESP32 ✅")
             st.metric("Soil Moisture", soil)
 
             if soil > 3000:
-                st.warning("Dry Soil")
+                st.warning("🌵 Dry Soil")
             else:
-                st.success("Soil Good")
+                st.success("💧 Soil Good")
+
+        else:
+            st.error("Server Error")
 
     except Exception as e:
         st.error(f"Connection Failed: {e}")
